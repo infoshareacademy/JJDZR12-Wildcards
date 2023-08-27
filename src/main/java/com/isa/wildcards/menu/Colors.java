6c2705c91e40bbfb291baf78a8d760d1db841385
Smrj319 <dariusz.zander@gmail.com>
2023-08-27 10:34:20 +0200

    changed Colors class to ENUM

--- src/main/java/com/isa/wildcards/menu/Colors.java
+++ src/main/java/com/isa/wildcards/menu/Colors.java
@@ -1,12 +1,21 @@
 package com.isa.wildcards.menu;
 
-public class Colors {
-    static String red = "\u001B[31m";
-    static String green = "\u001B[32m";
-    static String yellow = "\u001B[33m";
-    static String blue = "\u001B[34m";
-    static String magenta = "\u001B[35m";
-    static String cyan = "\u001B[36m";
-    static String reset = "\u001B[0m";
+public enum Colors {
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    YELLOW("\u001B[33m"),
+    BLUE("\u001B[34m"),
+    MAGENTA("\u001B[35m"),
+    CYAN("\u001B[36m"),
+    RESET("\u001B[0m");
 
+    private final String code;
+
+    Colors(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
--- src/main/java/com/isa/wildcards/menu/MenuUtils.java
+++ src/main/java/com/isa/wildcards/menu/MenuUtils.java
@@ -5,48 +5,48 @@ public class MenuUtils {
 
 
     public static void printOptionsMain() {
-        System.out.println(Colors.cyan + "Main Menu:" + Colors.reset);
-        System.out.println(Colors.cyan + "1. Search with login" + Colors.reset);
-        System.out.println(Colors.cyan + "2. Search without login" + Colors.reset);
-        System.out.println(Colors.cyan + "3. Create user" + Colors.reset);
-        System.out.println(Colors.cyan + "4. Exit \n" + Colors.reset);
+        System.out.println(Colors.CYAN.getCode() + "Main Menu:" + Colors.RESET.getCode());
+        System.out.println(Colors.CYAN.getCode() + "1. Search with login" + Colors.RESET.getCode());
+        System.out.println(Colors.CYAN.getCode() + "2. Search without login" + Colors.RESET.getCode());
+        System.out.println(Colors.CYAN.getCode() + "3. Create user" + Colors.RESET.getCode());
+        System.out.println(Colors.CYAN.getCode() + "4. Exit \n" + Colors.RESET.getCode());
 
-        System.out.println(Colors.green + "If you select search with login, you will be provided with these options: \n" +
+        System.out.println(Colors.GREEN.getCode() + "If you select search with login, you will be provided with these options: \n" +
                 "- Search \n" +
-                "- Search history \n" + Colors.reset);
-        System.out.println(Colors.green + "If you select search without login, you will be provided with these options: \n" +
-                "- Search \n" + Colors.reset);
-        System.out.print(Colors.yellow + "Enter your choice (1-4): " + Colors.reset);
+                "- Search history \n" + Colors.RESET.getCode());
+        System.out.println(Colors.GREEN.getCode() + "If you select search without login, you will be provided with these options: \n" +
+                "- Search \n" + Colors.RESET.getCode());
+        System.out.print(Colors.YELLOW.getCode() + "Enter your choice (1-4): " + Colors.RESET.getCode());
     }
 
     public static void printOptionsLoginMenu() {
-        System.out.println(Colors.magenta + "Login Menu:" + Colors.reset);
-        System.out.println(Colors.magenta + "1. Login" + Colors.reset);
-        System.out.println(Colors.magenta + "2. Return to Main Menu \n" + Colors.reset);
-        System.out.print(Colors.yellow + "Enter your choice (1-2): " + Colors.reset);
+        System.out.println(Colors.MAGENTA.getCode() + "Login Menu:" + Colors.RESET.getCode());
+        System.out.println(Colors.MAGENTA.getCode() + "1. Login" + Colors.RESET.getCode());
+        System.out.println(Colors.MAGENTA.getCode() + "2. Return to Main Menu \n" + Colors.RESET.getCode());
+        System.out.print(Colors.YELLOW.getCode() + "Enter your choice (1-2): " + Colors.RESET.getCode());
     }
 
     public static void printOptionsSubMenuTwo() {
-        System.out.println(Colors.magenta + "Search without login" + Colors.reset);
-        System.out.println(Colors.magenta + "1. Search" + Colors.reset);
-        System.out.println(Colors.magenta + "2. Return to Main Menu \n" + Colors.reset);
-        System.out.println(Colors.yellow + "Enter your choice (1-2): " + Colors.reset);
+        System.out.println(Colors.MAGENTA.getCode() + "Search without login" + Colors.RESET.getCode());
+        System.out.println(Colors.MAGENTA.getCode() + "1. Search" + Colors.RESET.getCode());
+        System.out.println(Colors.MAGENTA.getCode() + "2. Return to Main Menu \n" + Colors.RESET.getCode());
+        System.out.println(Colors.YELLOW.getCode() + "Enter your choice (1-2): " + Colors.RESET.getCode());
     }
 
     public static void printOptionsSubMenuOne() {
-        System.out.println(Colors.magenta + "Search with login:" + Colors.reset);
-        System.out.println(Colors.magenta + "1. Search" + Colors.reset);
-        System.out.println(Colors.magenta + "2. Show search history" + Colors.reset);
-        System.out.println(Colors.magenta + "3. Return to Login Menu \n" + Colors.reset);
-        System.out.println(Colors.yellow + "Enter your choice (1-3): " + Colors.reset);
+        System.out.println(Colors.MAGENTA.getCode() + "Search with login:" + Colors.RESET.getCode());
+        System.out.println(Colors.MAGENTA.getCode() + "1. Search" + Colors.RESET.getCode());
+        System.out.println(Colors.MAGENTA.getCode() + "2. Show search history" + Colors.RESET.getCode());
+        System.out.println(Colors.MAGENTA.getCode() + "3. Return to Login Menu \n" + Colors.RESET.getCode());
+        System.out.println(Colors.YELLOW.getCode() + "Enter your choice (1-3): " + Colors.RESET.getCode());
     }
     public static void invalidChoice() {
-        System.out.println(Colors.red + "Invalid choice, please select again" + Colors.reset);
+        System.out.println(Colors.RED.getCode() + "Invalid choice, please select again" + Colors.RESET.getCode());
     }
     public static void wrongInput() {
-        System.out.println(Colors.red + "Invalid input. You need to enter a number!" + Colors.reset);
+        System.out.println(Colors.RED.getCode() + "Invalid input. You need to enter a number!" + Colors.RESET.getCode());
     }
     public static void returning() {
-        System.out.println(Colors.yellow + "Returning to Menu" + Colors.reset);
+        System.out.println(Colors.YELLOW.getCode() + "Returning to Menu" + Colors.RESET.getCode());
     }
 }
