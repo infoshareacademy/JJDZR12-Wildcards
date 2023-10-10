8a83f0beb55267582b7344a6b674b4fe3645910d
Smrj319 <dariusz.zander@gmail.com>
2023-10-10 22:13:47 +0200

    cleaned up code

--- JJDZR12-WildcardsWeb/src/main/java/com/isa/wildcards/controller/SearchPageController.java
+++ JJDZR12-WildcardsWeb/src/main/java/com/isa/wildcards/controller/SearchPageController.java
@@ -2,7 +2,6 @@ package com.isa.wildcards.controller;
 
 import com.isa.wildcards.dto.MovieDto;
 import com.isa.wildcards.sevice.SearchResultService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
