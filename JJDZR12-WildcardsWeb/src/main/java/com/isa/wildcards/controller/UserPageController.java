914ea066d4d1c59a0e6c7c7a37d51c1e7fbfa46f
Krzysztof Harezlak <km.harezlak@gmail.com>
2023-11-04 08:26:43 +0100

    user panel added

--- JJDZR12-WildcardsWeb/src/main/java/com/isa/wildcards/controller/UserPageController.java
+++ JJDZR12-WildcardsWeb/src/main/java/com/isa/wildcards/controller/UserPageController.java
@@ -1,13 +1,32 @@
 package com.isa.wildcards.controller;
 
+import com.isa.wildcards.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserPageController {
     @GetMapping("/user")
     public String getMainUserPage() {
         return "user-main-page";
+    }
+    @GetMapping("/registration")
+    public String getRegistrationPage() {
+        return "registration-page";
+    }
+    @GetMapping("/sign-in")
+    public String getSignInPage() {
+        return "sign-in-page";
+    }
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute("newUser") UserDto userDto){
+        if (userDto.getUserName().equals("Username") && userDto.getPassword().equals("password")){
+            System.out.println("Welcome" + userDto.getUserName());
+        }
+
+        return "redirect:/";
     }
 }
 
