6c7d2544017ccdd1c9b01f1aa071a4ddf87e4b52
Krzysztof Harezlak <km.harezlak@gmail.com>
2023-11-05 13:07:20 +0100

    registration page added

---
+++ JJDZR12-WildcardsWeb/src/main/java/com/isa/wildcards/dto/UserDto.java
@@ -0,0 +1,25 @@
+package com.isa.wildcards.dto;
+
+public class UserDto {
+    private String userName;
+    private String password;
+
+    public UserDto() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
