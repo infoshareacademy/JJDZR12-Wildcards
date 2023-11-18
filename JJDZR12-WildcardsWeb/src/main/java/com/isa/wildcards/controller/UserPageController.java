75e25b7dc24769fa4e37c3063e193cc961032ed8
Krzysztof Harezlak <km.harezlak@gmail.com>
2023-11-18 08:10:18 +0100

    changes from comments on GitHub

--- JJDZR12-WildcardsWeb/src/main/java/com/isa/wildcards/controller/UserPageController.java
+++ JJDZR12-WildcardsWeb/src/main/java/com/isa/wildcards/controller/UserPageController.java
@@ -1,21 +1,21 @@
 package com.isa.wildcards.controller;
 
-import com.isa.wildcards.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserPageController {
+
     @GetMapping("/user")
     public String getMainUserPage() {
         return "user-main-page";
     }
+
     @GetMapping("/registration")
     public String getRegistrationPage() {
         return "registration-page";
     }
+
     @GetMapping("/sign-in")
     public String getSignInPage() {
         return "sign-in-page";
