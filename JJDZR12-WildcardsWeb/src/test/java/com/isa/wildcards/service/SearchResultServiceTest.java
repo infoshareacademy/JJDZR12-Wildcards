6ba1e7444e2cc12c2346f097c0130a1f2672ab01
Smrj319 <dariusz.zander@gmail.com>
2024-02-05 19:02:16 +0100

    finished all tasks

--- JJDZR12-WildcardsWeb/src/test/java/com/isa/wildcards/service/SearchResultServiceTest.java
+++ JJDZR12-WildcardsWeb/src/test/java/com/isa/wildcards/service/SearchResultServiceTest.java
@@ -51,8 +51,6 @@ class SearchResultServiceTest {
         assertThat(result).usingRecursiveComparison().isEqualTo(expected);
     }
 
-
-
     private Movie createSampleMovie(String title, String year, String rated, String released, String runtime, String genre,
                                     String director, String writer, String actors, String plot, String language, String country,
                                     String awards, String metascore) {
